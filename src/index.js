9a5faeb627a6eff99641d2d824fd1c79ec10ab60
Alextibtab <48660540+Alextibtab@users.noreply.github.com>
2021-07-28 17:49:11 +0100

    Fixed syntax errors and enabled strict mode for JS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws'); 
+
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server  = express()
-    .use((req, res) => res.sendfile(INDEX, { root: __dirname }))
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const { server } = require('ws'); 
 
 const wss = new Server({ server })
 
